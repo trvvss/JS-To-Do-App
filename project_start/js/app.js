9ced9b11bd97f1a5b759bc95444b6a85ec78b5ff
Travis Nelson <travis.nelson.ca.us@gmail.com>
2016-09-06 13:01:01 -0700

    Project Start

---
+++ project_start/js/app.js
@@ -0,0 +1,8 @@
+//Problem: User interactiion doesn't provide desired results.
+//Solution: Add interactivity so the user can manage daily tasks.
+
+//Add a new task
+//Edit an existing task
+//Mark a task as complete
+//Mark a task as incomplete
+//
